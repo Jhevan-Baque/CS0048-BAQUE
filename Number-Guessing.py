5a2b110ce5e35b0f3430708b8b3d3ed4b852ca92
Jhevan Baque <aerovan.dev@gmail.com>
2025-05-07 12:44:59 +0800

    Update Number-Guessing.py

--- Number-Guessing.py
+++ Number-Guessing.py
@@ -7,16 +7,20 @@ def NumberGuessing():
     attempts = 0
     while True:
         os.system('cls' if os.name == 'nt' else 'clear')
-        print("1. Play Number Guessing Game\n2. Exit")
+        print("=" * 26)
+        print("   NUMBER GUESSING GAME")
+        print("=" * 26)
+        print("1. Play\n2. Exit")
         choice = input("Enter your choice (1-2): ")
         if choice == "1":
             random_int = random.randint(1, 100)
-            print(f"The Number is {random_int}")
             while True:
                 guess = input("Guess the number (1-100): ")
                 if int(guess) == random_int:
                     attempts += 1
-                    print(f"You Guessed the number in {attempts} attempt(s).")
+                    print("=" * 45)
+                    print(f"   You Guessed the number in {attempts} attempt(s).")
+                    print("=" * 45)
                     pause()
                     break
                 elif int(guess) > random_int:
