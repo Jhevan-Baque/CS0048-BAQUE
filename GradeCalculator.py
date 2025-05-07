4335508f76f4bf686ef8d600089887a8495fe9b2
Jhevan Baque <aerovan.dev@gmail.com>
2025-05-07 12:29:12 +0800

    Update GradeCalculator.py

--- GradeCalculator.py
+++ GradeCalculator.py
@@ -3,28 +3,42 @@ import os
 def pause():
     input("Press Enter to continue")
 
-def NumberGuessing():
-    numofsubj = 0
-    avg = 0
+def GradeCalculator():
+    records = {}
     while True:
         os.system('cls' if os.name == 'nt' else 'clear')
         print("1. Add Score\n2. Calculate Average\n3. Exit")
         choice = input("Enter your choice (1-3): ")
         if choice == "1":
             subj = input("Enter the subject: ")
-            numofsubj += 1
-            score = input("Enter the score: ")
-            avg += float(score)
-            pause()
+            if subj in records:
+                print(f"{subj}, Already Exists, Do you want to replace? \n1: Yes\n2: No")
+                yorn = input("Enter your choice: ")
+                if yorn == "1":
+                    score = input("Enter the score: ")
+                    records[str(subj)] = float(score)
+                    pause()
+                elif yorn == "2":
+                    continue
+                else:
+                    print("Invalid Choice")
+                    continue
+            else:
+                score = input("Enter the score: ")
+                records[str(subj)] = float(score)
+                pause()
         elif choice == "2":
-            print(f"Average Grade: {float(avg/numofsubj)}")
+            avg = 0
+            for _,v in records.items():
+                avg += v
+            print(f"Average Grade: {avg/len(records)}")
             pause()
         elif choice == "3":
             print("Thankyou for using Grade Calculator")
-            pause()
+            break
         else:
             print("Invalid Choice!")
             pause()
             continue
 
-NumberGuessing()
+GradeCalculator()
