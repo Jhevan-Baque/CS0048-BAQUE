51dc95f263ad7e86d868958003c958e3ddb64168
Jhevan Baque <aerovan.dev@gmail.com>
2025-05-07 11:13:10 +0800

    Create SimpleCalculator.py

---
+++ SimpleCalculator.py
@@ -0,0 +1,40 @@
+import os
+
+def pause():
+    input("Press Enter to continue")
+
+def SimpleCalculator():
+    while True:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Exit")
+        choice = input("Enter your choice (1-5): ")
+        if choice in ('1', '2', '3', '4'):
+            x = input("Enter first number: ")
+            if not x.isnumeric():
+                print("Invalid Input")
+                pause()
+                continue
+            y = input("Enter second number: ")
+            if not y.isnumeric():
+                print("Invalid Input")
+                pause()
+                continue
+            if choice == "1":
+                result = int(x)+int(y)
+            elif choice == "2":
+                result = int(x)-int(y)
+            elif choice == "3":
+                result = int(x)*int(y)
+            elif choice == "4":
+                result = int(x)/int(y)
+            print('Result: ' + str(result))
+            pause()
+        elif choice == "5":
+            print("Thankyou for using Simple Calculator!")
+            break
+        else:
+            print("Invalid Choice!")
+            pause()
+            continue
+
+SimpleCalculator()
